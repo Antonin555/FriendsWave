16f88f514710e9b3e69416131c7b66451865017d
Antonin555 <antonin.lenoir.molina@gmail.com>
2023-03-09 19:20:00 -0500

    Fragment Home Kodein et ViewModel + DataBinding

--- dev/app/src/main/java/com/antonin/friendswave/ui/fragment/HomeFragment.kt
+++ dev/app/src/main/java/com/antonin/friendswave/ui/fragment/HomeFragment.kt
@@ -5,18 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
+import androidx.lifecycle.ViewModelProviders
 import com.antonin.friendswave.R
-import com.antonin.friendswave.data.model.User
+import com.antonin.friendswave.databinding.FragmentHomeBinding
+import com.antonin.friendswave.ui.viewModel.HomeFragmentVMFactory
+import com.antonin.friendswave.ui.viewModel.HomeFragmentViewModel
 import org.kodein.di.Kodein
+
 import org.kodein.di.KodeinAware
-import org.kodein.di.android.kodein
+
 import org.kodein.di.generic.instance
+import org.kodein.di.android.x.kodein
 
 class HomeFragment : Fragment(), KodeinAware {
 
 
 
+    override val kodein : Kodein by kodein()
 
+
+    private val factory : HomeFragmentVMFactory by instance()
+//    private lateinit var progressbar : ProgressBar
+
+
+    private lateinit var viewModel: HomeFragmentViewModel
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +41,17 @@ class HomeFragment : Fragment(), KodeinAware {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding  = inflate(inflater, R.layout.fragment_home, container, false)
+        viewModel = ViewModelProviders.of(this,factory).get(HomeFragmentViewModel::class.java)
+        binding.item = viewModel
+        return binding.root
     }
 
-    override val kodein: Kodein
-        get() = TODO("Not yet implemented")
+
 
 
 }
+
+
+
+
