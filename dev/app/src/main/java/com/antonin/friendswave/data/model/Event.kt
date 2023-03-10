928660de652f39c5425196a707f31f15216cccda
Antonin555 <antonin.lenoir.molina@gmail.com>
2023-03-10 18:55:29 -0500

    Data Class Event rajout de données

--- dev/app/src/main/java/com/antonin/friendswave/data/model/Event.kt
+++ dev/app/src/main/java/com/antonin/friendswave/data/model/Event.kt
@@ -3,6 +3,10 @@ package com.antonin.friendswave.data.model
 data class Event(
 
     var name: String? = "",
+    var isPublic : Boolean? = true,
+    var nbrePersonnes : Int? = 0,
+    var categorie : String? = "",
+    var description : String? = "",
     var date: String? = "",
     var isActive: Boolean = false,
     var lattitude : String = "",
