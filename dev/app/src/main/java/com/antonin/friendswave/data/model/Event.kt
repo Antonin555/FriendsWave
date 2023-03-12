3c1dd20767439d53a14982d5b43227a6a47a23a3
Antonin555 <antonin.lenoir.molina@gmail.com>
2023-03-12 17:35:44 -0400

    MAJ eventFragment, recyclerFragment

--- dev/app/src/main/java/com/antonin/friendswave/data/model/Event.kt
+++ dev/app/src/main/java/com/antonin/friendswave/data/model/Event.kt
@@ -7,10 +7,10 @@ data class Event(
     var name: String? = "",
     var isPublic : Boolean? = true,
     var nbrePersonnes : Int? = 0,
-//    var categorie : String? = "",
-//    var description : String? = "",
-//    var date: String? = "",
-//    var lattitude : String = "",
-//    var longitude : String = ""
+    var categorie : String? = "",
+    var description : String? = "",
+    var date: String? = "",
+    var lattitude : String = "",
+    var longitude : String = ""
 
 ):ListItemViewModel()
